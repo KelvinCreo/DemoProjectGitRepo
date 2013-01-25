5b254ae29c6c6f6a5f45dff7d0d0f5883b577664
Kelvin <kelvin@creoagency.com>
2013-01-25 15:12:51 -0800

    Added Build Script

---
+++ BuildApp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Tell jenkins where the tools are
+export PATH=${PATH}:/Users/Kelvin/adt-bundle-mac-x86_64/sdk/tools:/Users/Kelvin/adt-bundle-mac-x86_64/sdk/platform-tools
+export ANDROID_HOME=/Users/Kelvin/adt-bundle-mac-x86_64/sdk
+
+#Build my app
+cd /Users/Kelvin/.jenkins/workspace/DemoProject/DemoProject
+rm -f build.xml
+android update project -p . -t android-14 -sdk
+
+#Update the tests
+cd /Users/Kelvin/.jenkins/workspace/DemoProject/DemoProjectTest
+android update test-project -p . -m ../DemoProject
+
+exit
