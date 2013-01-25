321c084e77b7f8b38269a80acd22ca1010c373b8
Kelvin <kelvin@creoagency.com>
2013-01-25 15:57:18 -0800

    Added Acceptance Test Script Fixed

--- AcceptanceTests.sh
+++ AcceptanceTests.sh
@@ -2,6 +2,6 @@
 
 #Acceptance Tests
 cd /Users/Kelvin/.jenkins/DemoProject/DemoProject
-calabash-android run ./bin/MainActivity-debug.apk
+calabash-android run /Users/Kelvin/.jenkins/DemoProject/DemoProject/bin/MainActivity-debug.apk
 
 exit
